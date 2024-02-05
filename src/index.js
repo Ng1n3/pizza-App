054b900b9633c992ccfe20d4b707935a5c2dc08d
ng1n3 <code.with.muyiwa@gmail.com>
2024-02-05 20:19:03 +0100

    extracting jsx into a new component

--- src/index.js
+++ src/index.js
@@ -120,7 +120,7 @@ function Pizza(props) {
 function Footer() {
   // return React.createElement('footer', null, "we're currently open!")
   const hour = new Date().getHours();
-  const openHour = 12;
+  const openHour = 2;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <= closeHour;
 
@@ -128,19 +128,24 @@ function Footer() {
   // else alert("Sorry we're closed");
   return (
     <footer className="footer">
-      {isOpen ? (
-        <div className="order">
-          <p>We're Open until {closeHour}:00. Come visit us or order onine</p>
-          <button className="btn">Order</button>
-        </div>
-      ) : (
+      {isOpen ? <Order closeHour={closeHour}/>: (
         <p>
-          we're happy to welcome you between the hours of {openHour}:00 and{" "}
+          we're happy to welcome you between the hours of {openHour}:00 and
           {closeHour}:00.
         </p>
       )}
     </footer>
   );
+}
+
+function Order(props) {
+  return 
+  <div className="order"> 
+    <p>
+      We're open until {props.closeHour}:00. Come visit us or order onine.
+    </p>
+    <button className="btn">Order</button>
+  </div>
 }
 
 // React v18
